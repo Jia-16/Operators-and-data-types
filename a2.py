8db469df6c0ad769fd0f543568f38a8113216dd2
Jia-16 <jkreviewsthings@gmail.com>
2026-01-07 20:18:06 +1100

    operators

---
+++ a2.py
@@ -0,0 +1,18 @@
+num1 = 53
+num2 = 16
+
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition  :", num1+num2)
+print("Difference. :", num1-num2)
+print("Product. :", num1*num2)
+print("Division. :", num1/num2)
+print("Floor division. :", num1//num2)
+print("Modulus Operation. :", num1%num2)
+print("Square. :", num2**2)
+print("Square Root  :", num2**0.5)
+
+print("Equal  ?", num1==num2)
+print("Number 1 greater?", num1>num2)
+print("Number 2 greater?", num1<num2)
+print("Not Equal", num1!=num2)
